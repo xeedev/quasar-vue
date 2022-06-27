2f51d1c5c0190a05269e7730a67b620c8c77dc63
xeemax <xeemax2.0@gmail.com>
2022-06-27 23:58:03 +0500

    ui changes in dashboard

--- src/layouts/DashboardLayout.vue
+++ src/layouts/DashboardLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <q-header elevated class="bg-black">
       <q-toolbar>
         <q-btn
           flat
@@ -11,15 +11,34 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <q-toolbar-title> Wood Surface </q-toolbar-title>
+        <q-space></q-space>
+        <q-btn color="dark" label="Logout" @click="$router.push('/login')">
+          <!-- <q-menu>
+            <q-list style="min-width: 100px">
+              <q-item clickable v-close-popup @click="$router.push('/login')">
+                <q-item-section>Logout</q-item-section>
+              </q-item>
+              <q-separator />
+            </q-list>
+          </q-menu> -->
+        </q-btn>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header>
+          <q-img
+            src="http://woodsurface.pk/jewelry/img/logo.png"
+            spinner-color="white"
+            @click="$router.push('/dashboard')"
+            fit="contain"
+            height="80px"
+            class="q-ma-sm cursor-pointer"
+            style="max-width: 150px"
+          />
+        </q-item-label>
 
         <EssentialLink
           v-for="link in essentialLinks"
@@ -41,46 +60,28 @@ import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
   {
-    title: 'Docs',
+    title: 'Dashboard',
     caption: 'quasar.dev',
     icon: 'school',
     link: 'https://quasar.dev',
   },
   {
-    title: 'Github',
+    title: 'Products',
     caption: 'github.com/quasarframework',
     icon: 'code',
     link: 'https://github.com/quasarframework',
   },
   {
-    title: 'Discord Chat Channel',
+    title: 'Orders',
     caption: 'chat.quasar.dev',
     icon: 'chat',
     link: 'https://chat.quasar.dev',
   },
   {
-    title: 'Forum',
+    title: 'Media',
     caption: 'forum.quasar.dev',
     icon: 'record_voice_over',
     link: 'https://forum.quasar.dev',
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev',
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev',
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev',
   },
 ];
 
--- src/pages/DashboardPage.vue
+++ src/pages/DashboardPage.vue
@@ -1,22 +1,13 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <q-page class="row items-center justify-evenly"> Dashboard </q-page>
 </template>
 
 <script lang="ts">
 import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  name: 'IndexPage',
-  components: { ExampleComponent },
+  name: 'DashboardPage',
   setup() {
     const todos = ref<Todo[]>([
       {
