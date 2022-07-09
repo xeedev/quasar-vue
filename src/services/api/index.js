de30ba5f09431b0538ddd31984bfa143a81114fc
xeemax <xeemax2.0@gmail.com>
2022-07-10 03:46:41 +0500

    added gallery module in services

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -26,6 +26,7 @@ const routes = {
         'statistics' : 'statistics',
         'categories' : 'categories',
         'orders' : 'orders',
+        'gallery' : 'gallery',
         'image-upload' : 'imageUpload',
     },
 };
