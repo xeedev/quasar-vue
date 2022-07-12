527a93c4d5f86219924ecc30a03788fd19403057
xeemax <xeemax2.0@gmail.com>
2022-07-12 23:02:36 +0500

    implmeneted categorywise products on frontend

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,7 +6,7 @@
       v-model="slide"
       thumbnails
       infinite
-      height="100vh"
+      height="70vh"
     >
       <q-carousel-slide
         v-for="(image,key) in images"
@@ -16,101 +16,57 @@
       />
     </q-carousel>
     <div>
-      <q-tabs v-model="tab" align="justify" narrow-indicator class="q-mb-lg">
-        <template v-for="(category, index) in categories" :key="index">
+      <h5 class="text-center text-bold">Our Products</h5>
+      <q-tabs v-model="tab" @click="updateProducts" align="justify" narrow-indicator class="q-mb-lg">
+        <template v-for="(category, index) in categoriess" :key="index">
           <q-tab
             class="text-black"
-            :name="category.key"
-            :label="category.value"
+            :name="category"
+            :label="category"
           />
         </template>
       </q-tabs>
 
       <div class="q-gutter-y-sm">
-        <q-tab-panels
-          v-model="tab"
-          animated
-          transition-prev="scale"
-          transition-next="scale"
-          class="text-white text-center"
-        >
-          <template v-for="(category, index) in categories" :key="index">
-            <q-tab-panel :name="category.key">
-              <div class="text-h6">{{ category.value }}</div>
-              <div class="row justify-center">
-                <template
-                  v-for="(image, index) in category.images"
-                  :key="index"
-                >
-                  <q-card
-                    class="my-card q-ma-md cursor-pointer"
-                    @click="$router.push('/three')"
-                  >
-                    <q-img :src="image" spinner-color="black" />
+        <div class="row justify-center">
+          <template
+            v-for="(category, index) in categories"
+            :key="index"
+          >
+            <q-card
+              class="text-dark my-card q-ma-md cursor-pointer"
+              @click="$router.push('/three')"
+            >
+              <q-img :src="category?.media[0]?.url" spinner-color="black" />
 
-                    <q-card-section>
-                      <q-btn
-                        fab
-                        color="primary"
-                        icon="place"
-                        class="absolute"
-                        style="top: 0; right: 12px; transform: translateY(-50%)"
-                      />
+              <q-card-section>
+                <q-btn
+                  fab
+                  color="red"
+                  icon="favorite"
+                  class="absolute"
+                  style="top: 0; right: 12px; transform: translateY(-50%)"
+                />
 
-                      <div class="row no-wrap items-center">
-                        <div class="col text-h6 ellipsis">
-                          Cafe Basilico ({{ index }})
-                        </div>
-                        <div
-                          class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-                        >
-                          <q-icon name="place" />
-                          250 ft
-                        </div>
-                      </div>
+                <div class="row no-wrap items-center">
+                  <div class="col text-h6 ellipsis">
+                    {{category.name}}
+                  </div>
+                </div>
 
-                      <q-rating v-model="stars" :max="5" size="32px" />
-                    </q-card-section>
+                <q-rating v-model="stars" :max="5" size="32px" />
+              </q-card-section>
 
-                    <q-card-section class="q-pt-none">
-                      <div class="text-subtitle1">$・Italian, Cafe</div>
-                      <div class="text-caption text-grey">
-                        Small plates, salads & sandwiches in an intimate
-                        setting.
-                      </div>
-                    </q-card-section>
+              <q-separator />
 
-                    <q-separator />
-
-                    <q-card-actions>
-                      <q-btn flat round icon="event" />
-                      <q-btn flat color="primary"> Reserve </q-btn>
-                    </q-card-actions>
-                  </q-card>
-                </template>
-              </div>
-            </q-tab-panel>
+              <q-card-actions>
+                <q-btn flat round icon="event" />
+                <q-btn flat color="primary"> Read More</q-btn>
+              </q-card-actions>
+            </q-card>
           </template>
-        </q-tab-panels>
+        </div>
       </div>
-      <q-btn
-        color="black"
-        class="full-width"
-        label="Get All"
-        @click="getAll()"
-      />
-      <q-btn
-        color="red"
-        class="full-width"
-        label="Get Detail"
-        @click="getDetail(1)"
-      />
-      <q-btn
-        color="yellow"
-        class="full-width text-black"
-        label="Post"
-        @click="postText()"
-      />
     </div>
   </div>
   <div class="container">
@@ -145,6 +101,7 @@ export default defineComponent({
   name: 'IndexPage',
   components: { ContactUs },
   setup() {
+    const products = ref([]);
     const images = ref([]);
     const todos = ref<Todo[]>([
       {
@@ -172,176 +129,34 @@ export default defineComponent({
       totalCount: 1200,
     });
     const slide = ref(1);
-    const categories = ref<Category[]>([
-      {
-        id: 1,
-        type: 'ct1',
-        key: 'a',
-        value: 'A',
-        images: [
-          'https://picsum.photos/200',
-          'https://picsum.photos/201',
-          'https://picsum.photos/202',
-          'https://picsum.photos/203',
-          'https://picsum.photos/204',
-          'https://picsum.photos/205',
-          'https://picsum.photos/206',
-          'https://picsum.photos/207',
-        ],
-      },
-      {
-        id: 2,
-        type: 'ct2',
-        key: 'b',
-        value: 'B',
-        images: [
-          'https://picsum.photos/208',
-          'https://picsum.photos/209',
-          'https://picsum.photos/210',
-          'https://picsum.photos/213',
-          'https://picsum.photos/214',
-          'https://picsum.photos/215',
-          'https://picsum.photos/216',
-          'https://picsum.photos/217',
-        ],
-      },
-      {
-        id: 3,
-        type: 'ct3',
-        key: 'c',
-        value: 'C',
-        images: [
-          'https://picsum.photos/220',
-          'https://picsum.photos/221',
-          'https://picsum.photos/222',
-          'https://picsum.photos/223',
-          'https://picsum.photos/224',
-          'https://picsum.photos/225',
-          'https://picsum.photos/226',
-          'https://picsum.photos/227',
-        ],
-      },
-      {
-        id: 4,
-        type: 'ct4',
-        key: 'd',
-        value: 'D',
-        images: [
-          'https://picsum.photos/230',
-          'https://picsum.photos/231',
-          'https://picsum.photos/232',
-          'https://picsum.photos/233',
-          'https://picsum.photos/234',
-          'https://picsum.photos/235',
-          'https://picsum.photos/236',
-          'https://picsum.photos/237',
-        ],
-      },
-      {
-        id: 5,
-        type: 'ct5',
-        key: 'e',
-        value: 'E',
-        images: [
-          'https://picsum.photos/230',
-          'https://picsum.photos/231',
-          'https://picsum.photos/232',
-          'https://picsum.photos/233',
-          'https://picsum.photos/234',
-          'https://picsum.photos/235',
-          'https://picsum.photos/236',
-          'https://picsum.photos/237',
-        ],
-      },
-      {
-        id: 1,
-        type: 'ct1',
-        key: 'f',
-        value: 'F',
-        images: [
-          'https://picsum.photos/200',
-          'https://picsum.photos/201',
-          'https://picsum.photos/202',
-          'https://picsum.photos/203',
-          'https://picsum.photos/204',
-          'https://picsum.photos/205',
-          'https://picsum.photos/206',
-          'https://picsum.photos/207',
-        ],
-      },
-      {
-        id: 2,
-        type: 'ct2',
-        key: 'g',
-        value: 'G',
-        images: [
-          'https://picsum.photos/240',
-          'https://picsum.photos/241',
-          'https://picsum.photos/242',
-          'https://picsum.photos/243',
-          'https://picsum.photos/244',
-          'https://picsum.photos/245',
-          'https://picsum.photos/246',
-          'https://picsum.photos/247',
-        ],
-      },
-      {
-        id: 3,
-        type: 'ct3',
-        key: 'h',
-        value: 'H',
-        images: [
-          'https://picsum.photos/250',
-          'https://picsum.photos/251',
-          'https://picsum.photos/252',
-          'https://picsum.photos/253',
-          'https://picsum.photos/254',
-          'https://picsum.photos/255',
-          'https://picsum.photos/256',
-          'https://picsum.photos/257',
-        ],
-      },
-      {
-        id: 4,
-        type: 'ct4',
-        key: 'i',
-        value: 'I',
-        images: [
-          'https://picsum.photos/260',
-          'https://picsum.photos/261',
-          'https://picsum.photos/262',
-          'https://picsum.photos/263',
-          'https://picsum.photos/264',
-          'https://picsum.photos/265',
-          'https://picsum.photos/266',
-          'https://picsum.photos/267',
-        ],
-      },
-      {
-        id: 5,
-        type: 'ct5',
-        key: 'j',
-        value: 'J',
-        images: [
-          'https://picsum.photos/270',
-          'https://picsum.photos/271',
-          'https://picsum.photos/272',
-          'https://picsum.photos/273',
-          'https://picsum.photos/274',
-          'https://picsum.photos/275',
-          'https://picsum.photos/276',
-          'https://picsum.photos/277',
-        ],
-      },
-    ]);
+    const tab = ref('all');
+    const categories = ref([]);
+    const categoriess = ref(['all']);
     const url = process.env.ROOT_URL + '/public/media/1657039301-9RXEN7lTHA.png';
     const testUrl = url.replace("public/", "storage/");
     const $q = useQuasar();
+    function getCategories(xs : any, key: any) {
+      return [...new Set(xs.map(item => item.category))];
+    }
+    function updateProducts(){
+      categories.value = [];
+      categories.value = tab.value === 'all' ? products.value : products.value.filter( product => product.category == tab.value );
+    }
     onMounted(async () => {
       let res = await Api.getList('gallery');
       res.data.data.forEach(i => {
         images.value.push(i.url)
       })
+      let response = await Api.getList('products')
+      products.value = response.data.data
+      categories.value = response.data.data
+      // tab.value = categories.value[0].category
+      let cat = getCategories(products.value,'category')
+      cat.forEach(c=>{
+        categoriess.value.push(c)
+      })
+
+      console.log('categoriess.value',categoriess.value)
     })
     return {
       testUrl,
@@ -349,9 +164,12 @@ export default defineComponent({
       meta,
       slide,
       images,
-
+      products,
+      categoriess,
+      getCategories,
+      updateProducts,
+      tab,
       stars: ref(5),
-      tab: ref('a'),
       categories,
       async getAll() {
         $q.loading.show();
